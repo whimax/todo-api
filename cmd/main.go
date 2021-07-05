f13ca7eb69b89c669c91758dd1861a5d88a37c2d
Maks White <whimax86@gmail.com>
2021-07-05 11:35:43 +0300

    Внедрение зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	todo "todo-app/pkg"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
