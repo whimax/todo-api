fb74ebe1a19ceacf150cca0b96c9a00ac218696f
Maks White <whimax86@gmail.com>
2021-07-05 17:39:08 +0300

    auth handlers

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	todo "todo-app/pkg"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+
+	}
 }
 
 func (h *Handler) signIn(c *gin.Context) {
