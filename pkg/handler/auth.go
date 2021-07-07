c85b9dcdc7f6c7b47e2dfaf882a30f70e51206dd
Maks White <whimax86@gmail.com>
2021-07-07 12:22:45 +0300

    user registration

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	todo "todo-app/pkg"
 )
 
@@ -9,8 +10,20 @@ func (h *Handler) signUp(c *gin.Context) {
 	var input todo.User
 
 	if err := c.BindJSON(&input); err != nil {
-
+		NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
 	}
+
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+
 }
 
 func (h *Handler) signIn(c *gin.Context) {
